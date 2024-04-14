1394fd85f0f7c1abf6a1d4821a5227f13ade181b
Nitesh Silwal <67178303+niteshsilwal01@users.noreply.github.com>
2024-04-14 08:20:04 +0545

    Update VGGNet19.py

--- VGGNet19.py
+++ VGGNet19.py
@@ -1,7 +1,7 @@
 from keras.applications.vgg19 import VGG19
 
 def VGGNet19Model():
-    # Load the VGG16 model
+    # Load the model
     vgg19_model = VGG19(weights='imagenet')
     return vgg19_model
     
