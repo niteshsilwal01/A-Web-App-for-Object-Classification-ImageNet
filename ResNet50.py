db2d968b09a9616c31ca4d07b26eda126d7f7dc9
Nitesh Silwal <67178303+niteshsilwal01@users.noreply.github.com>
2024-04-14 08:19:45 +0545

    Update ResNet50.py

--- ResNet50.py
+++ ResNet50.py
@@ -1,6 +1,6 @@
 from keras.applications.resnet50 import ResNet50
 
 def ResNet50Model():
-    # Load the VGG16 model
+    # Load the model
     resnet50_model = ResNet50(weights='imagenet')
     return resnet50_model
